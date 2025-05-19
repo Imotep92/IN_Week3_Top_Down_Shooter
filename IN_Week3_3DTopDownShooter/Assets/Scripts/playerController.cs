6c1bd518effb2c4d0146c44978a454fbf7d70848
Imotep92 <Imogyn.nicholas@yahoo.co.uk>
2025-05-19 17:20:23 +0100

    2nd commit

--- IN_Week3_3DTopDownShooter/Assets/Scripts/playerController.cs
+++ IN_Week3_3DTopDownShooter/Assets/Scripts/playerController.cs
@@ -10,7 +10,14 @@ public class playerController : MonoBehaviour
     //setting player lateral movement control
     public float horizontalInput;
 
+    public float forwardInput;
+
     public float xRange = 21;
+
+    public float turnSpeed;
+
+    //public float zRange = 0;
+
 
 
     void Start()
@@ -21,11 +28,16 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //input for lateral moevement control is 'A' and 'D'(only one player profile)
+        //input for lateral movement control is 'A' and 'D'(only one player profile)
         horizontalInput = Input.GetAxis("Horizontal");
+
+        //input for vertical movement control is 'W' and 'S'(only one player profile)
+        forwardInput = Input.GetAxis("Vertical");
 
         //move along x axis     
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //transform.Rotate(Vector3.)
 
         //Keep player in bounds(-xRange => Player <= xRange) exceeding boundaries will transform position to new
         if (transform.position.x < -xRange)
@@ -37,7 +49,16 @@ public class playerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        //move along z axis     
+        transform.Translate(Vector3.forward * forwardInput * Time.deltaTime * speed);
+
+        
     }
+
+
 }
+
+
 
    
