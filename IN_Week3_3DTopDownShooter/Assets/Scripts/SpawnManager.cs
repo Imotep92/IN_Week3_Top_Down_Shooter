e7c2c4762d5bb6afe846bbddbe2d895ab3560ed1
Imotep92 <Imogyn.nicholas@yahoo.co.uk>
2025-05-22 02:03:56 +0100

    4th Commit
    - imported wooden textures
    - made Fences Prefabs

--- IN_Week3_3DTopDownShooter/Assets/Scripts/SpawnManager.cs
+++ IN_Week3_3DTopDownShooter/Assets/Scripts/SpawnManager.cs
@@ -4,47 +4,29 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
 
-
-    // OLD CODE
-    /* private float spawnRangeX = 20.0f;
-       private float spawnPosZ = 20.0f;
-       */
-
     private float xRange = 25.0f;
     private float zRange = 15.0f;
 
     public animalInfo[] animalInformation;
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
-
     // Update is called once per frame
     void Update()
     {
-        //OLD CODE
-        /* if (Input.GetKeyDown(KeyCode.L))
-          {
-
-              //randomly spawn animal index and spawn position
-              Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
-              int animalIndex = Random.Range(0, animalPrefabs.Length);
-
-              Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-          }*/
-
-
-
         if (Input.GetKeyDown(KeyCode.L))
         {
-
             int animalIndex = Random.Range(0, animalPrefabs.Length);
             Vector3 spawnPosition = new Vector3(Random.Range(-xRange, xRange), 0, Random.Range(zRange, -zRange));
             Instantiate(animalPrefabs[animalIndex], spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log(animalInformation[0].animalName);
         }
     }
 
