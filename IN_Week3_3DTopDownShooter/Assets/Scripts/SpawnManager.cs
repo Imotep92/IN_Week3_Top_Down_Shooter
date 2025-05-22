db2f0fa1f7a863ec44d3659b444333bd38a8cd9b
Imotep92 <Imogyn.nicholas@yahoo.co.uk>
2025-05-22 11:51:47 +0100

    5th commit, 22/05/25. 11:50
    - animal spawn is automatic
    - applied box colliders to all animals and player character

--- IN_Week3_3DTopDownShooter/Assets/Scripts/SpawnManager.cs
+++ IN_Week3_3DTopDownShooter/Assets/Scripts/SpawnManager.cs
@@ -9,25 +9,33 @@ public class SpawnManager : MonoBehaviour
 
     public animalInfo[] animalInformation;
 
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+       InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
 
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPosition = new Vector3(Random.Range(-xRange, xRange), 0, Random.Range(zRange, -zRange));
-            Instantiate(animalPrefabs[animalIndex], spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0));
-        }
-
+       /*
         if (Input.GetKeyDown(KeyCode.F))
         {
             Debug.Log(animalInformation[0].animalName);
-        }
+        }*/
+    }
+
+    void SpawnRandomAnimal()
+    { 
+        int animalIndex = Random.Range(0, animalPrefabs.Length); Vector3 spawnPosition = new Vector3(Random.Range(-xRange, xRange), 0, Random.Range(zRange, -zRange));
+
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0));
+
     }
 
     [System.Serializable]
